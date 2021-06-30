b9473c19f49066dfefdd28e3c5701befe764772a
Vadim <dr.vasja@gmail.com>
2021-06-30 13:06:09 +0000

    Improving styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,43 +3,44 @@ class PagesController < ApplicationController
   def contacts
     @contacts = [
       {
-        avatar: 'andrey.jpg',
-        name_label: 'Имя: ',
-        name: 'Andrey',
-        email_label: 'email: ',
-        email: 'andrey@gmail.com',
-        tel_label: 'tel: ',
-        tel: '+375777777777',
-        tel_link: 'tel:375777777777'
-      },
-      {
-        avatar: 'vadim.jpg',
-        name_label: 'Имя: ',
-        name: 'Vadim',
+
+        email_ico: 'email-256.png',
         email_label: 'email: ',
         email: 'vadim@gmail.com',
+        tel_ico: 'phone-256.png',
         tel_label: 'tel: ',
-        tel: '+375777777777',
-        tel_link: 'tel:375777777777'
-      },{
-        avatar: 'dina.jpg',
-        name_label: 'Имя: ',
-        name: 'Dina',
-        email_label: 'email: ',
-        email: 'dina@gmail.com',
-        tel_label: 'tel: ',
-        tel: '+375777777777',
-        tel_link: 'tel:375777777777'
-      },{
-        avatar: 'lina.jpg',
-        name_label: 'Имя: ',
-        name: 'Lina',
-        email_label: 'email: ',
-        email: 'lina@gmail.com',
-        tel_label: 'tel: ',
-        tel: '+375777777777',
-        tel_link: 'tel:375777777777'
+        tel: '+375333062942',
+        tel_link: 'tel:+375333062942'
       }
+      
+      # {
+      #   avatar: 'vadim.jpg',
+      #   name_label: 'Имя: ',
+      #   name: 'Vadim',
+      #   email_label: 'email: ',
+      #   email: 'vadim@gmail.com',
+      #   tel_label: 'tel: ',
+      #   tel: '+375777777777',
+      #   tel_link: 'tel:375777777777'
+      # },{
+      #   avatar: 'dina.jpg',
+      #   name_label: 'Имя: ',
+      #   name: 'Dina',
+      #   email_label: 'email: ',
+      #   email: 'dina@gmail.com',
+      #   tel_label: 'tel: ',
+      #   tel: '+375777777777',
+      #   tel_link: 'tel:375777777777'
+      # },{
+      #   avatar: 'lina.jpg',
+      #   name_label: 'Имя: ',
+      #   name: 'Lina',
+      #   email_label: 'email: ',
+      #   email: 'lina@gmail.com',
+      #   tel_label: 'tel: ',
+      #   tel: '+375777777777',
+      #   tel_link: 'tel:375777777777'
+      # }
     ]
   end
 
